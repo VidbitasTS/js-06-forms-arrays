033163cbbc803535f6740e0e84da44ed97a84e72
vidbitas <vidbitas@gmail.com>
2022-07-13 13:38:20 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,38 @@
+< !DOCTYPE html >
+    <
+    html lang = "en" >
+
+    <
+    head >
+    <
+    meta charset = "UTF-8" / >
+    <
+    meta http - equiv = "X-UA-Compatible"
+content = "IE=edge" / >
+    <
+    meta name = "viewport"
+content = "width=device-width, initial-scale=1.0" / >
+    <
+    title > JS 06 < /title> <
+    link rel = "stylesheet"
+href = "./css/style.css" >
+    <
+    script src = "./js/main.js"
+defer > < /script> <
+    /head>
+
+<
+body > < /body> <
+    h1 > New project < /h1>
+
+<
+/html>  eilute = eilute + 2;
+//     }
+//     return rezString;
+//   } else {
+//     rezString = `blogas aukstis ${aukstis} (${Math.round(aukstis)})`;
+//     return rezString;
+//   }
+// }
+
+// console.log(createTree(7));
